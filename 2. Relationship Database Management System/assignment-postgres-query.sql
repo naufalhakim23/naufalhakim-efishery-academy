6333837200ec6c81bec6ea11213617c821cf4f10
naufalhakim <hakimnaufal212@gmail.com>
2022-09-21 20:13:43 +0700

    feat: inserting data into customers, products and orders

--- 2. Relationship Database Management System/assignment-postgres-query.sql
+++ 2. Relationship Database Management System/assignment-postgres-query.sql
@@ -16,3 +16,26 @@ create table if not exists orders (
 	order_date timestamp not null,
 	total float not null
 );
+-- Inserting data into customers
+insert into customers(customer_name) values('Lewis');
+insert into customers(customer_name) values('Nikita');
+insert into customers(customer_name) values('Johan');
+insert into customers(customer_name) values('Verstrappen');
+insert into customers(customer_name) values('Sebastian');
+
+-- Inserting data into products
+insert into products(name) values('Ikan Tongkol');
+insert into products(name) values('Ikan Lele');
+insert into products(name) values('Ikan Mas');
+insert into products(name) values('Ikan Patin');
+insert into products(name) values('Ikan Kakap');
+
+-- Inserting data into orders
+insert into orders(customer_id, product_id, order_date, total) values(1, 5, now(), 100);
+insert into orders(customer_id, product_id, order_date, total) values(2, 4, now(), 200);
+insert into orders(customer_id, product_id, order_date, total) values(3, 3, now(), 300);
+insert into orders(customer_id, product_id, order_date, total) values(4, 2, now(), 400);
+insert into orders(customer_id, product_id, order_date, total) values(5, 1, now(), 500);
+-- or shorthand
+insert into orders(customer_id, product_id, order_date, total) 
+values(1, 5, now(), 100), (2, 4, now(), 200), (3, 3, now(), 300), (4, 2, now(), 400), (5, 1, now(), 500);
