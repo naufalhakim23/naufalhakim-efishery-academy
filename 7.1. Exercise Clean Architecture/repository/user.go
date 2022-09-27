758eae1c3974d157b20cdc26d9c7e4a979ffe510
rpo <hakimnaufal212@gmail.com>
2022-09-27 21:28:29 +0700

    feat: reading all data from database

--- 7.1. Exercise Clean Architecture/repository/user.go
+++ 7.1. Exercise Clean Architecture/repository/user.go
@@ -8,6 +8,7 @@ import (
 
 type InterfaceUserRepository interface {
 	Store(user entity.User) (entity.User, error)
+	FindAll() ([]entity.User, error)
 }
 type UserRepository struct {
 	db *gorm.DB
@@ -16,8 +17,20 @@ type UserRepository struct {
 func NewUserRepository(db *gorm.DB) InterfaceUserRepository {
 	return &UserRepository{db}
 }
+
+// Storing data to database
 func (r UserRepository) Store(user entity.User) (entity.User, error) {
 	if err := r.db.Debug().Create(&user).Error; err != nil {
+		return entity.User{}, err
 	}
 	return user, nil
+}
+
+// Read data from database
+func (r UserRepository) FindAll() ([]entity.User, error) {
+	var users []entity.User
+	if err := r.db.Debug().Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
 }
