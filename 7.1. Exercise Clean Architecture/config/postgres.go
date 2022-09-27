70fbd1dd64d0c49aad5eb31716977209d0dd0c87
rpo <hakimnaufal212@gmail.com>
2022-09-27 19:06:23 +0700

    feat: connecting to database and migrating to db

---
+++ 7.1. Exercise Clean Architecture/config/postgres.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"exercise-clean-architecture/entity"
+	"fmt"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+var err error
+
+func Connect() {
+	DB, err = gorm.Open(postgres.Open(os.Getenv("db_url")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Database connected")
+}
+
+func Migrate() {
+	DB.AutoMigrate(&entity.User{})
+}
