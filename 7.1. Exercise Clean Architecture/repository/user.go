e4120251a7cc2649dfd2332de77e2398545ae4b6
rpo <hakimnaufal212@gmail.com>
2022-09-28 08:24:47 +0700

    feat: adding connection to database find users information from uuid

--- 7.1. Exercise Clean Architecture/repository/user.go
+++ 7.1. Exercise Clean Architecture/repository/user.go
@@ -9,6 +9,7 @@ import (
 type InterfaceUserRepository interface {
 	Store(user entity.User) (entity.User, error)
 	FindAll() ([]entity.User, error)
+	FindByUUID(uuid string) (entity.User, error)
 }
 type UserRepository struct {
 	db *gorm.DB
@@ -33,4 +34,13 @@ func (r UserRepository) FindAll() ([]entity.User, error) {
 		return nil, err
 	}
 	return users, nil
+}
+
+// Find user using uuid from database
+func (r UserRepository) FindByUUID(uuid string) (entity.User, error) {
+	var user entity.User
+	if err := r.db.Debug().Where("uuid = ?", uuid).First(&user).Error; err != nil {
+		return entity.User{}, err
+	}
+	return user, nil
 }
