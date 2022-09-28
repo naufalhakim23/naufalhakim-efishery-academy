e46edfc2e4669faa0cbb65621f5059a2023b1a62
rpo <hakimnaufal212@gmail.com>
2022-09-28 11:16:39 +0700

    feat: adding user update user request

--- 7.1. Exercise Clean Architecture/entity/user.go
+++ 7.1. Exercise Clean Architecture/entity/user.go
@@ -1,6 +1,8 @@
 package entity
 
-import "time"
+import (
+	"time"
+)
 
 type User struct {
 	ID         int       `json:"id"`
@@ -9,14 +11,14 @@ type User struct {
 	LastName   string    `json:"last_name"`
 	Username   string    `json:"username"`
 	Email      string    `json:"email"`
-	Password   string    `json:"password"`
+	Password   string    `json:"password" `
 	Phone      string    `json:"phone"`
 	Role       string    `json:"role"`
 	CreatedAt  time.Time `json:"created_at"`
 	ModifiedAt time.Time `json:"modified_at"`
 }
 
-type UserRequest struct {
+type CreateUserRequest struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Username  string `json:"username"`
@@ -24,8 +26,18 @@ type UserRequest struct {
 	Password  string `json:"password"`
 	Phone     string `json:"phone"`
 }
+
+type UpdateUserRequest struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	Phone     string `json:"phone"`
+	Password  string `json:"password"`
+}
 type UserResponse struct {
 	ID        int    `json:"id"`
+	UUID      string `json:"uuid"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Username  string `json:"username"`
