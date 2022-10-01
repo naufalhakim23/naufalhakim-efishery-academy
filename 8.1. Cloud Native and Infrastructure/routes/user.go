793aba7b27a186c58c6e877bf2a99d8c5b90db5c
rpo <hakimnaufal212@gmail.com>
2022-10-01 14:30:54 +0700

    chore: copy file from exercise to try making a Dockerized filesystem

---
+++ 8.1. Cloud Native and Infrastructure/routes/user.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"exercise-clean-architecture/handler"
+
+	"github.com/labstack/echo/v4"
+)
+
+func Routes(e *echo.Echo, userHandler *handler.UserHandler) {
+	e.POST("/users", userHandler.CreateUser)
+	e.GET("/users", userHandler.GetAllUser)
+	e.GET("/users/:uuid", userHandler.GetUserByUUID)
+	e.PUT("/users/:uuid", userHandler.UpdateUserByUUID)
+	e.DELETE("/users/:uuid", userHandler.DeleteUserByUUID)
+}
