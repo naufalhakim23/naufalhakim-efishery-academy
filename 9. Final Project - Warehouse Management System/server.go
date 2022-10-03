26b3c789e9388ccb8bb96660827a5c7299fb3c44
rpo <hakimnaufal212@gmail.com>
2022-10-03 11:48:36 +0700

    feat: serving for warehouse endpoints

---
+++ 9. Final Project - Warehouse Management System/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	config "warehouse-management-system-eFishery/config/database"
+	"warehouse-management-system-eFishery/handler"
+	"warehouse-management-system-eFishery/repository"
+	"warehouse-management-system-eFishery/routes"
+	"warehouse-management-system-eFishery/services"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	config.Connect()
+	config.Migrate()
+
+	e := echo.New()
+	warehouseRepository := repository.NewWarehouseRepository(config.DB)
+	warehouseService := services.NewWarehouseService(warehouseRepository)
+	warehouseHandler := handler.NewWarehouseHandler(warehouseService)
+
+	routes.Routes(e, warehouseHandler)
+	e.Logger.Fatal(e.Start(":8080"))
+}
