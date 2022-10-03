5afebaa5cd6ddb5280c9a2ea2cce0dd1dd4f8643
rpo <hakimnaufal212@gmail.com>
2022-10-03 10:23:27 +0700

    feat: adding configuration for postgres database with gorm

---
+++ 9. Final Project - Warehouse Management System/config/database/postgres.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	supplier "warehouse-management-system-eFishery/entity/supplier"
+	warehouse "warehouse-management-system-eFishery/entity/warehouse"
+
+	_ "github.com/joho/godotenv/autoload"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+var err error
+
+func Connect() {
+	DB, err = gorm.Open(postgres.Open(os.Getenv("db_url")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Database connected")
+}
+
+func Migrate() {
+	DB.AutoMigrate(&warehouse.WarehouseWorkers{})
+	DB.AutoMigrate(&warehouse.WarehouseRoles{})
+	DB.AutoMigrate(&warehouse.WarehouseAuth{})
+	DB.AutoMigrate(&warehouse.Warehouse{})
+	DB.AutoMigrate(&warehouse.WarehouseAddresses{})
+	DB.AutoMigrate(&warehouse.WarehouseOrders{})
+	DB.AutoMigrate(&warehouse.WarehouseProducts{})
+	DB.AutoMigrate(&warehouse.WarehouseCategories{})
+
+	DB.AutoMigrate(&supplier.Supplier{})
+	DB.AutoMigrate(&supplier.SupplierAddress{})
+
+}
